d9c9314fa66039075e0cb02ca6b18fcc606beb58
Max Sim <ultimaxslc@gmail.com>
2012-05-10 14:55:10 +0800

    Header done. Just before form basics

--- index2b.php
+++ index2b.php
@@ -64,7 +64,7 @@ $app_info = $facebook->api('/'. AppInfo::appID());
 
 $app_name = idx($app_info, 'name', '');
 
-?>
+?> <!-- php closed -->
 
 <!DOCTYPE html>
 <html xmlns:fb="http://ogp.me/ns/fb#" lang="en">
@@ -136,7 +136,52 @@ $app_name = idx($app_info, 'name', '');
 	      }(document, 'script', 'facebook-jssdk'));
 	    </script>
 
-	    
+	    <header class="clearfix">
+        <?php if (isset($basic)) { ?>
+        <p id="picture" style="background-image: url(https://graph.facebook.com/<?php echo he($user_id); ?>/picture?type=normal)"></p>
+
+        <div>
+          <h1>Welcome, <strong><?php echo he(idx($basic, 'name')); ?></strong></h1>
+          <p class="tagline">
+            This is your app
+            <!-- @max App name + Link all in one code -->
+            <a href="<?php echo he(idx($app_info, 'link'));?>" target="_top"><?php echo he($app_name); ?></a>
+          </p>
+      
+          <!-- !!!!!!!!!!!!!!!!!!!!!!!!!!-->
+          <!--        SHARE BUTTONS      -->  
+          <!-- !!!!!!!!!!!!!!!!!!!!!!!!!!-->
+
+          <div id="share-app">
+            <p>Share your app:</p>
+            <ul>
+              <li>
+                <a href="#" class="facebook-button" id="postToWall" data-url="<?php echo AppInfo::getUrl(); ?>">
+                  <span class="plus">Post to Wall</span>
+                </a>
+              </li>
+              <li>
+                <a href="#" class="facebook-button speech-bubble" id="sendToFriends" data-url="<?php echo AppInfo::getUrl(); ?>">
+                  <span class="speech-bubble">Send Message</span>
+                </a>
+              </li>
+              <li>
+                <a href="#" class="facebook-button apprequests" id="sendRequest" data-message="Test this awesome app">
+                  <span class="apprequests">Send Requests</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <?php } else { ?>
+
+        
+        <div>
+          <h1>Welcome</h1>
+          <div class="fb-login-button" data-scope="user_likes,user_photos"></div>
+        </div>
+        <?php } ?>
+    </header>
 
 
 
