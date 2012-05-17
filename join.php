d5cf54f0845f4933bb312d5973f2fd1eec50e63a
Max Sim <ultimaxslc@gmail.com>
2012-05-17 16:44:42 +0800

    able to draw basic information from graph API

--- join.php
+++ join.php
@@ -1,4 +1,66 @@
-<!-- ?php
+<?php
+	
+	// Provides access to app specific values such as your app id and app secret.
+	// Defined in 'AppInfo.php'
+	require_once('AppInfo.php');
+
+	// Enforce https on production
+	if (substr(AppInfo::getUrl(), 0, 8) != 'https://' && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
+	  header('Location: https://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+	  exit();
+	}
+
+	// This provides access to helper functions defined in 'utils.php'
+	require_once('utils.php');
+
+
+	/*****************************************************************************
+	 *
+	 * The content below provides examples of how to fetch Facebook data using the
+	 * Graph API and FQL.  It uses the helper functions defined in 'utils.php' to
+	 * do so.  You should change this section so that it prepares all of the
+	 * information that you want to display to the user.
+	 *
+	 ****************************************************************************/
+
+	require_once('sdk/src/facebook.php');
+
+	$facebook = new Facebook(array(
+	  'appId'  => AppInfo::appID(),
+	  'secret' => AppInfo::appSecret(),
+	));
+	
+
+
+
+
+
+	$basic = $facebook->api('/ultimaxslc');
+
+
+
+
+
+
+
+
+
+	$user_id = $facebook->getUser();
+	if ($user_id){
+		try {
+		    // Fetch the viewer's basic information
+		    $basic = $facebook->api('/ultimaxslc');
+		  } catch (FacebookApiException $e) {
+		    // If the call fails we check if we still have a user. The user will be
+		    // cleared if the error is because of an invalid accesstoken
+		    if (!$facebook->getUser()) {
+		      header('Location: '. AppInfo::getUrl($_SERVER['REQUEST_URI']));
+		      exit();
+		    }
+		}	
+	}	
+
+	/*
 	$host = ""; //hostname
 	$id = ""; //userid
 	$f_name = "";
@@ -6,13 +68,11 @@
 	$email = "";
 	$occupation = "";
 	$country = "";
-	$howknow = "";
-	$id = "";
+	//
 	$contactno = "";
 	
-	$submission = "";
-	$datetime = "";
-	$screenshot ="";
+	//$submission = "";
+	//$datetime = "";
 
 	// we need to initialize and use these information eventually because
 	// these info are not stored locally on the db
@@ -60,7 +120,7 @@
  	*/
  ?>
 
--->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -75,8 +135,45 @@
 	<!--script src="js/forms.js"></script>
 	<!-- This imports the css file. -->
 	<link href="stylesheets/bootstrap.css" rel="stylesheet">
+	<!-- script language="JavaScript" src="scripts/gen_validatorv31.js" type="text/javascript"></script-->	
+
 </head>
 
+
+<body>
+<script type="text/javascript">
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '<?php echo AppInfo::appID(); ?>', // App ID
+          channelUrl : '//<?php echo $_SERVER["HTTP_HOST"]; ?>/channel.html', // Channel File
+          status     : true, // check login status
+          cookie     : true, // enable cookies to allow the server to access the session
+          xfbml      : true // parse XFBML
+        });
+
+        // Listen to the auth.login which will be called when the user logs in
+        // using the Login button
+        FB.Event.subscribe('auth.login', function(response) {
+          // We want to reload the page now so PHP can read the cookie that the
+          // Javascript SDK sat. But we don't want to use
+          // window.location.reload() because if this is in a canvas there was a
+          // post made to this page and a reload will trigger a message to the
+          // user asking if they want to send data again.
+          window.location = window.location;
+        });
+
+        FB.Canvas.setAutoGrow();
+      };
+
+      // Load the SDK Asynchronously
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
 
 
 <table width="300" border="0" align="center" cellpadding="6" cellspacing="1" bgcolor="#CCCCCC">
@@ -100,22 +197,22 @@
 				</tr>
 				<tr>
 					<td colspan = "2">
-					<label class="control-label" for="f_name"> First Name:</label> <input class="input-xlarge" name="f_name" type="text" id="f_name" required="required" value="From Graph API">
+					<label class="control-label" for="f_name"> First Name:</label> <input class="input-xlarge" name="f_name" type="text" id="f_name" required="required" value="<?php echo he(idx($basic, 'first_name')); ?>">
 					</td>
-					<td colspan = "2"><label class="control-label" for="l_name"> Last Name:</label> <input class="input-xlarge" name="l_name" type="text" id="l_name" required="required" value="From Graph API"></td>
+					<td colspan = "2"><label class="control-label" for="l_name"> Last Name:</label> <input class="input-xlarge" name="l_name" type="text" id="l_name" required="required" value="<?php echo he(idx($basic, 'last_name')); ?>"></td>
 				</tr>
 
 				<tr>
 					<td colspan = "2">
 					<label class="control-label" for="occupation"> Occupation:</label> <input class="input-xlarge" name="occupation" type="text" required="required" id="occupation">
 					</td>
-					<td><label class="control-label" for="email"> Email:</label> <input class="input-xlarge" name="email" type="email" id="email" required="required" value="From Graph API"></td>
+					<td><label class="control-label" for="email"> Email:</label> <input class="input-xlarge" name="email" type="email" id="email" required="required" value="<?php echo he(idx($basic, 'email')); ?>"></td>
 
 				</tr>
 
 				<tr>
 					<td colspan = "2">
-					<label class="control-label" for="country"> Country:</label> <input class="input-xlarge" name="country" type="text" id="country" required="required" value="From Graph API">
+					<label class="control-label" for="country"> Country:</label> <input class="input-xlarge" name="country" type="text" id="country" required="required" value="<?php echo he(idx($basic, 'location')); ?>">
 					</td>
 					<td><label class="control-label" for="contactno"> Contact No:</label> <input class="input-xlarge" name="contactno" type="text" required="required" id="contactno" value=""></td>
 
@@ -150,19 +247,7 @@
 	</tr>
 
 
-</table>
-
-<script> 
-	$("submission").validator(); 
-	// initialize validator for a bunch of input fields
-  	var inputs = $("#form :input").validator();
- 
-  	// perform validation programmatically
-  	inputs.data("validator").checkValidity();
+</table>  
+</body>
 
 
-
-</script>
-
-
-  
